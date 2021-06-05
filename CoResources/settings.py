36770eddda7b2420dd677764ebcbb5c0b8034537
GagandeepRamgiri <Gagandeepramgiri@gmail.com>
2021-06-05 06:07:30 +0530

    Update settings.py

--- CoResources/settings.py
+++ CoResources/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-++q*suk_it2-h+l=3i2me-0w5l*qn239dr23!i7my!luhjboz#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','https://coresources.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1','coresources.herokuapp.com']
 
 
 # Application definition
